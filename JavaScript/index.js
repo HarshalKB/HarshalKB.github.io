686d2863dae599873fb075c16171c165d57400e1
Harshal Bombarde <harshalbombarde@gmail.com>
2024-02-05 05:53:34 +0530

    first commit

---
+++ JavaScript/index.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".typing", {
+    strings:["Frontend Developer", "Backend Developer", "Python Developer", "Software Developer", "Fullstack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
